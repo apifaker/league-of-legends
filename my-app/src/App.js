cc926c149e8f38230be6427066361c4f7e8e7a5d
Pudim Dog <pw.arcadiawr@gmail.com>
2023-05-30 18:16:33 -0300

    att footer e header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,20 @@
 import './App.css';
-import { useCallback } from 'react/cjs/react.production.min';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
-  const url = 'https://league-of-legends-champions.p.rapidapi.com/champions/pt-br?page=0&size=10&name=aatrox&role=fighter';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '6795b45346msh226b82303e4e28ep108d74jsn9f0ed758028e',
-      'X-RapidAPI-Host': 'league-of-legends-champions.p.rapidapi.com'
-    }
-  };
-
-  const loader = useCallback(async () => {
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [])
-
-
-  useEffect(() => {
-    loader()
-  }, []);
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer/>
+    </>
+  )
 }
 
 export default App;
---
+++ my-app/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+
+	// https://rapidapi.com/snldnc-kpCtDKbxo_F/api/league-of-legends-champions/
+	
+	return (
+		<div>
+			<h1>Titulo 1</h1>
+
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, dolorem quam tempore soluta inventore doloribus totam praesentium cumque sint maiores vero excepturi, repudiandae placeat, consequatur corporis. Dicta a obcaecati error?</p>
+		</div>
+	)
+}
+
+export default Home
